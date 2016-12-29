59332fca196163477a64b1f7ce24fe9c22169a42
Loïc Dutrieux <loic.dutrieux@gmail.com>
2016-12-30 00:14:49 +0100

    First complete version of extract wrapper

--- src/satmo/wrappers.py
+++ src/satmo/wrappers.py
@@ -1,10 +1,14 @@
 from datetime import datetime, timedelta
 import itertools
+import multiprocessing.dummy as mp
+import functools
+import os
+from pprint import pprint
+import warnings
+
 from .query import query_from_extent, make_download_url
 from .download import download_robust
 from .utils import file_path_from_sensor_date
-import warnings
-from pprint import pprint
 
 
 def timerange_download(sensors, begin, end, write_dir,\
@@ -53,14 +57,55 @@ def timerange_download(sensors, begin, end, write_dir,\
     return local_files_list
 
     # test = satmo.timerange_download(['tm'], '2005-01-01', '2005-01-06', '/home/loic/sandbox/dl_tests', north = 33, south = 10, west = -133, east = -110)
-def extract_wrapper(input_dir, north, south, west, east, compress, clean, pattern,\
-                    overwrite = False, keep_uncompressed = False):
-    pass
+def extract_wrapper(input_dir, north, south, west, east, compress = True, clean = True, init_kwargs = {},\
+                    extract_kwargs = {}, compress_kwargs = {}, clean_kwargs = {}):
+    """Wrapper for performing spatial extraction on L1A data
+
+    Performs data unpacking (implicit), extraction, repacking (optional), and cleaning of
+    junk files.
+
+    Args:
+        input_dir (str): path of directory that contains the data
+        north (float): north latitude of bounding box in DD
+        south (float): south latitude of bounding box in DD
+        west (float): west longitude of bounding box in DD
+        east (float): east longitude of bounding box in DD
+        compress (bool): Should data be recompressed after extraction, defaults to True
+        clean (bool): Should intermediary output be deleted from directory,
+        defaults to True. If compress is set to False, and clean is set to True,
+        you should probably put sonething in the clean_kwargs argument
+        init_kwargs (dict): dictionary of arguments passed as **kwargs to the extractJob class instantation
+        'pattern' is the only one at the moment
+        extract_kwargs (dict): dictionary of arguments passed as **kwargs to the extractJob.extract()
+        method. 'overwrite' is the only one at the moment
+        compress_kwargs (dict): dictionary of arguments passed as **kwargs to the extractJob.compress()
+        method. 'overwrite' is the only one at the moment
+        clean_kwargs (dict): dictionary of arguments passed as **kwargs to the extractJob.clean()
+        method. 'keep_uncompressed' is the only one at the moment
+
+    Return:
+        A dict with 'input_dir' as key and status as value (0 for successful, 1 if crash
+        or directory did not exist)
+    """
+    if os.path.isdir(input_dir):
+        try:
+            ext = satmo.extractJob(input_dir, **init_kwargs)
+            ext.extract(north=north, south=south, west=west, east=east, **extract_kwargs)
+            if compress:
+                ext.compress(**compress_kwargs)
+            if clean:
+                ext.clean(**clean_kwargs)
+            return {input_dir: 0}
+        except Exception as e:
+            return {input_dir: 1}
+    else:
+        return {input_dir: 1}
+
 
 def timerange_extract(sensors, data_root, begin, end,\
                       north, south, west, east,\
                       overwrite = False, compress = True, clean = True,\
-                      n_threads = 1):
+                      keep_uncompressed = False, n_threads = 1):
     """Extract spatial extent for a given time range
 
     Wrapper around satmo.extractJob class
@@ -74,4 +119,14 @@ def timerange_extract(sensors, data_root, begin, end,\
     ## Make a list of all combinations to pass as args in list comprehension
     sensor_date_combo = list(itertools.product(sensors, date_range))
     dir_list = [file_path_from_sensor_date(*x, data_root = data_root) for x in sensor_date_combo]
+    kwargs = {'north': north,
+              'south': south,
+              'west': west,
+              'east': east,
+              'compress': compress,
+              'clean': clean,
+              'overwrite': overwrite}
+    pool = mp.Pool(3)
+    out = pool.map(functools.partial(extract_wrapper, **kwargs), dir_list)
+
 
